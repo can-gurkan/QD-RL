e862198555ba9d2386133d7e21ce262ad5205eb1
Can Gurkan <cangurkan@me.com>
2023-12-03 20:26:01 -0600

    Starting work on incorporating a pytorch neural net model.

---
+++ models.py
@@ -0,0 +1,27 @@
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class MLP(nn.Module):
+
+    def __init__(self, n_observations, n_actions, fc1_dims, fc2_dims):
+        super(MLP, self).__init__()
+        self.input_dims = n_observations
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.fc1 = nn.Linear(self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        actions = self.fc3(x)
+        return actions
+    
